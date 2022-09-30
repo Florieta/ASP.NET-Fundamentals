6b30343d918ab6ff49ebf6cd8cb6249353393e47
Florieta <florieta.pencheva@gmail.com>
2022-09-30 23:34:35 +0300

    Initial commit WebShop demo

---
+++ WebShop/WebShop.Core/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Core.Contracts;
+using WebShop.Core.Models;
+
+namespace WebShop.Core.Services
+{
+    /// <summary>
+    /// Manipulates product data
+    /// </summary>
+    public class ProductService : IProductService
+    {
+        private readonly IConfiguration config;
+        /// <summary>
+        /// IoC
+        /// </summary>
+        /// <param name="config">Application config</param>
+        public ProductService(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+
+        /// <summary>
+        /// Gets all product
+        /// </summary>
+        /// <returns>List of products</returns>
+        public async Task<IEnumerable<ProductDto>> GetAll()
+        {
+            string dataPath = config.GetSection("DataFiles:Products").Value;
+            string data = await File.ReadAllTextAsync(dataPath);
+
+            return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(data);
+        }
+
+
+    }
+}
